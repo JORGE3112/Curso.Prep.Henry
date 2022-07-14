df7aeb5cea468aedd350844859a6f6af4dc819dd
Jorgito <jorgeasalgado@hotmail.com>
2022-07-14 18:30:45 -0300

    2do commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "JORGE ARMANDO";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 31;
 
 // Crea una variable booleana:
 const nuevoBool = null;
